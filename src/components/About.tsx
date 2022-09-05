18eddbbad97ae675b28d9aa02586c0000ae66589
zachuri <punsalang.zachary@gmail.com>
2022-09-04 22:27:23 -0700

    about: setup about

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+	return (
+		<div className="w-full md:h-screen p-2 flex items-center py-16">
+			<div className="max-w-[1240px] m-auto md:grid grid-cols gap-8">
+				<div className="col-span-2">
+					<p>About</p>
+					<h2>Who am I</h2>
+					<p>//I am not your normal developer</p>
+				</div>
+				<div></div>
+			</div>
+		</div>
+	);
+}
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Main from "../components/Main";
 import Navbar from "../components/Navbar";
+import About from "../components/About";
 
 const Home: NextPage = () => {
 	return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
 			</Head>
 			<Navbar />
 			<Main />
+			<About />
 		</>
 	);
 };
