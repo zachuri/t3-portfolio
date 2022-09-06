583442d927acb63975b9616e46770f451a2267dd
zachuri <punsalang.zachary@gmail.com>
2022-09-06 14:14:34 -0700

    aboutPage: added profile image/ completed setup

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,47 @@
+import Image from "next/image";
 import React from "react";
 
 function About() {
 	return (
 		<div className="w-full md:h-screen p-2 flex items-center py-16">
-			<div className="max-w-[1240px] m-auto md:grid grid-cols gap-8">
+			<div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
 				<div className="col-span-2">
-					<p>About</p>
-					<h2>Who am I</h2>
-					<p>//I am not your normal developer</p>
-					<p>//I am not your normal developer</p>
+					<p className="uppercase text-xl tracking-widest text-[#5651e5]">
+						About
+					</p>
+					<h2 className="py-4">Who am I</h2>
+					<p className="py-2 text-gray-600">
+						/ / I am not your normal developer
+					</p>
+					<p className="py-2 text-gray-600">
+						I have spent the last 12 years in the fire service working as
+						a professional firefighter/paramedic. I have always had a
+						knack for technology and working with computers. In 2019 I
+						started working with HTML and CSS to make some minor edits on
+						a small business website that I was operating. What I thought
+						was just a few small edits turned into a love for programming.
+					</p>
+					<p className="py-2 text-gray-600">
+						Fascinated with how intricate programming can be I was quickly
+						drawn to learn more. I started learning javascript and was
+						even more enthused with making websites interactive. I then
+						started freelancing for e-commerce companies on the Shopify
+						platform. I am now spending my time building projects with
+						React JS, Firebase, and learning new technologies.
+					</p>
+					<p className="py-2 text-gray-600 underline cursor-pointer">
+						Check out some of my latest projects
+					</p>
 				</div>
-				<div></div>
+				<div className="w-full h-auto m-auto shadow-xl shadow-gray-400 flex items-center justify-center p-4 hover: scale-105 ease-in duration-300">
+					<Image
+						className="rounded-xl"
+						src="/assets/github-profile-pic.jpeg"
+						alt="picture of zachuri"
+						height="500px"
+						width="500px"
+					/>
+				</div>
 			</div>
 		</div>
 	);
