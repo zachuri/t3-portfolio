81fd1d37b00cc72c88d1c00400bf89088de07355
zachuri <punsalang.zachary@gmail.com>
2022-09-07 01:25:27 -0700

    added navbar to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import Navbar from "../components/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Navbar />
+			<Component {...pageProps} />;
+		</>
+	);
 };
 
 export default MyApp;
